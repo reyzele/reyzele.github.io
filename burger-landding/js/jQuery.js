70c315c70b75be887baf04c70539ad1cdce880a1
Vadim Veksler <reyzele91@gmail.com>
2018-01-19 01:19:59 +0200

    last updates

--- burger-landding/js/jQuery.js
+++ burger-landding/js/jQuery.js
@@ -5,3 +5,11 @@ jQuery(document).ready(function () {
     $("#hero").css("min-height", "650px")
   });
 });
+jQuery(document).ready(function (e) {
+  e(".scroll").click(function (t) {
+    t.preventDefault();
+    e("html,body").animate({
+      scrollTop: e(this.hash).offset().top
+    }, 1e3)
+  })
+});
