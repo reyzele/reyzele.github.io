93b2d7d5b687f1d6fdb41e2ef6505b9a157c4b8c
Vadim Veksler <reyzele91@gmail.com>
2018-01-25 20:10:37 +0200

    version without JS

--- burger-landding/js/jQuery.js
+++ burger-landding/js/jQuery.js
@@ -1,10 +1,3 @@
-jQuery(document).ready(function () {
-  $("#burger, #about-us, #reviews").height($(window).height());
-  $(window).resize(function () {
-    $("#burger, #about-us, #reviews").height($(window).height());
-    $("#burger, #about-us, #reviews").css("min-height", "650px")
-  });
-});
 jQuery(document).ready(function (e) {
   e(".scroll").click(function (t) {
     t.preventDefault();
