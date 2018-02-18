17f4c11e542ce8df5f4cacb78a1c04b0053e1c71
Vadim Veksler <reyzele91@gmail.com>
2018-02-18 23:02:02 +0200

    es

--- burger-landding/js/main-js.js
+++ burger-landding/js/main-js.js
@@ -103,9 +103,7 @@ $(document).ready(function (e) {
     $('#fullpage').fullpage({
       //Навигация
       menu: '#menu',
-      lockAnchors: true,
-      anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
-      lockAnchors: true,
+      anchors: ['hero', 'best', 'slider', 'team', 'menu', 'reviews', 'order', 'map'],
       navigation: true,
       navigationPosition: 'right',
       showActiveTooltip: true,
