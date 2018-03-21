0f1706410d484b444e5ced1a0a7572eaeec205a3
Vadim <vbulka1986@gmail.com>
2018-03-21 17:14:53 +0200

    Bug with Blur fix

--- portfolio/assets/scripts/common/blure.js
+++ portfolio/assets/scripts/common/blure.js
@@ -1,20 +1,34 @@
+
 $(document).ready(function () {
-  blur();
-})
+  setTimeout(blur, 500);
+});
+ 
+
 
 $(window).resize(function () {
   blur();
 });
 
-
 function blur() {
   var imgWidth = $('.about__background').width(),
+    imgMinWidth = $('.about__background'),
     blurSection = $('.about'),
+    blurSectionHeight = $('.about').height(),
     blur = $('.about__form'),
     posY = blurSection.offset().top - blur.offset().top, //   текущее положение элемента относительно документа.
     posX = blurSection.offset().left - blur.offset().left;
   blur.css({
     'background-size': imgWidth + 'px' + ' ' + 'auto',
     'background-position': posX + 'px' + ' ' + posY + 'px'
-  })
+  });
+  console.log(blurSectionHeight);
+  if (blurSectionHeight > 1376) {
+    console.log('Внутри!');
+    var minWidth;
+    minWidth=1800+((1800/1376)*((blurSectionHeight)-1376));
+    console.log(minWidth);
+    $('.about__background').css({
+      'min-width': minWidth + 'px'
+    });
+  }
 }
