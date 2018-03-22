1bfbf7c05d5783a40c85b4828d99035bcd593f75
Vadim <vbulka1986@gmail.com>
2018-03-22 22:59:16 +0200

    Works form fixing done

--- portfolio/assets/scripts/common/blure.js
+++ portfolio/assets/scripts/common/blure.js
@@ -11,7 +11,7 @@ function blur () {
   var imgMinWidth = $('.about__background');
   var blurSection = $('.about');
   var blurSectionHeight = $('.about').height();
-  var blur = $('.about__form');
+  var blur = $('.about__form-background');
   var posY = blurSection.offset().top - blur.offset().top; //   текущее положение элемента относительно документа.
   var posX = blurSection.offset().left - blur.offset().left;
   if (blurSectionHeight > 1376) {
