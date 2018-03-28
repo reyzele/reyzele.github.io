ea3a91c5afefa8841455a9ba088ecfbcf4efc725
Vadim <vbulka1986@gmail.com>
2018-03-28 15:31:21 +0300

    Welcome parallax done

---
+++ portfolio/assets/scripts/common/parallax.js
@@ -0,0 +1,24 @@
+var parallaxContainer = document.getElementById('welcome__parallax'),
+  layers = parallaxContainer.children;
+
+var moveLayers = function (e) {
+  var initialX = (window.innerWidth / 2) - e.pageX;
+  var initialY = (window.innerHeight / 2) - e.pageY;
+
+  [].slice.call(layers).forEach(function (layer, index) {
+    var
+      divider = index / 100,
+      positionX = initialX * divider,
+      positionY = initialY * divider,
+      Procent = window.innerHeight / 100 * 15;
+      bottomPosition = (window.innerHeight / 2) * divider + Procent,
+      transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+      image = layer.firstElementChild;
+
+    layer.style.transform = transformString;
+    image.style.bottom = '-' + bottomPosition + 'px';
+  });
+
+};
+
+window.addEventListener('mousemove', moveLayers);
