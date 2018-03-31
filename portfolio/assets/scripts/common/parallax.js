623a5e10963dc8616e78771f5c31d9439083867b
Vadim <vbulka1986@gmail.com>
2018-03-31 12:52:26 +0300

    Adapvive for tablets done works and about pages

--- portfolio/assets/scripts/common/parallax.js
+++ portfolio/assets/scripts/common/parallax.js
@@ -2,15 +2,19 @@ var parallaxContainer = document.getElementById('welcome__parallax'),
   layers = parallaxContainer.children;
 
 var moveLayers = function (e) {
+
   var initialX = (window.innerWidth / 2) - e.pageX;
   var initialY = (window.innerHeight / 2) - e.pageY;
+
+  // console.log(e.pageX);
+  // console.log(e.pageY);
 
   [].slice.call(layers).forEach(function (layer, index) {
     var
       divider = index / 100,
       positionX = initialX * divider,
       positionY = initialY * divider,
-      Procent = window.innerHeight / 100 * 15;
+      Procent = window.innerHeight / 100 * 20;
       bottomPosition = (window.innerHeight / 2) * divider + Procent,
       transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
       image = layer.firstElementChild;
