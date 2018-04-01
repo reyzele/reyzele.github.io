5862414c1ec0aec69ee5a35f6d465fcf2753f764
Vadim Veksler <reyzele91@gmail.com>
2018-04-02 01:37:13 +0300

    JS bundle done

---
+++ portfolio/assets/scripts/common/main-parallax.js
@@ -0,0 +1,22 @@
+var MainParallax = function (e) {
+  var parallaxContainer = document.getElementById('welcome__parallax'),
+    layers = parallaxContainer.children;
+  var initialX = (window.innerWidth / 2) - e.pageX;
+  var initialY = (window.innerHeight / 2) - e.pageY;
+
+  [].slice.call(layers).forEach(function (layer, index) {
+    var
+      divider = index / 100,
+      positionX = initialX * divider,
+      positionY = initialY * divider,
+      Procent = window.innerHeight / 100 * 20,
+      bottomPosition = (window.innerHeight / 2) * divider + Procent,
+      transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+      image = layer.firstElementChild;
+
+    layer.style.transform = transformString;
+    image.style.bottom = '-' + bottomPosition + 'px';
+  });
+
+};
+export { MainParallax };
---
+++ portfolio/assets/scripts/common/skills.js
@@ -0,0 +1,65 @@
+// Skills
+if ($('.skills').length > 0) {
+  class CreateSkill {
+    constructor(options) {
+      this.s = Snap(options.container);
+      const radius = options.width / 3;
+      const cx = options.width / 2;
+      const cy = options.height / 2;
+      this.length = 2 * radius * Math.PI;
+      this.percent = options.percent;
+      this.s.attr({
+        width: options.width,
+        height: options.height
+      });
+      this.baseCircle = this.s.circle(cx, cy, radius);
+      this.baseCircle.attr({
+        fill: 'none',
+        stroke: '#ccc',
+        strokeWidth: 15
+      });
+      this.bgCircle = this.s.circle(cx, cy, radius);
+      this.bgCircle.transform('r-90,' + cx + ',' + cy);
+      this.bgCircle.attr({
+        fill: 'none',
+        stroke: '#1bb696',
+        strokeWidth: 15,
+        strokeDasharray: this.length,
+        strokeDashoffset: this.length
+      });
+    }
+    draw() {
+      Snap.animate(this.length, (1 - this.percent) * this.length,
+        (val) => {
+          this.bgCircle.attr({
+            strokeDashoffset: val
+          });
+        }, 1500, mina.easeinout);
+    }
+  }
+
+  const html = new CreateSkill({ width: 110, height: 110, container: '#html', percent: 0.9 });
+  const css = new CreateSkill({ width: 110, height: 110, container: '#css', percent: 0.85 });
+  const js = new CreateSkill({ width: 110, height: 110, container: '#js', percent: 0.5 });
+  const php = new CreateSkill({ width: 110, height: 110, container: '#php', percent: 0.3 });
+  const mySQL = new CreateSkill({ width: 110, height: 110, container: '#mySQL', percent: 0.5 });
+  const node = new CreateSkill({ width: 110, height: 110, container: '#node', percent: 0.3 });
+  const mango = new CreateSkill({ width: 110, height: 110, container: '#mango', percent: 0.4 });
+  const git = new CreateSkill({ width: 110, height: 110, container: '#git', percent: 0.8 });
+  const gulp = new CreateSkill({ width: 110, height: 110, container: '#gulp', percent: 0.7 });
+  const bower = new CreateSkill({ width: 110, height: 110, container: '#bower', percent: 0.6 });
+
+  var skills = function () {
+    html.draw();
+    css.draw();
+    js.draw();
+    php.draw();
+    mySQL.draw();
+    node.draw();
+    mango.draw();
+    git.draw();
+    gulp.draw();
+    bower.draw();
+  }
+}
+export { skills };
